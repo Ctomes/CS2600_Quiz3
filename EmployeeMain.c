134e4f79da61742c0cd0d1bb038d01000613dd2f
Ctomes <72369162+Ctomes@users.noreply.github.com>
2022-10-19 19:53:56 -0700

    part1 is done.

--- EmployeeMain.c
+++ EmployeeMain.c
@@ -4,11 +4,14 @@
  
 #include <string.h> 
 #include <stdlib.h> 
-#include "employee.h" 
+#include "Employee.h" 
 int main(void){ 
     //defined in employeeSearchOne.c 
     PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind); 
     PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char * nameToFind); 
+    PtrToEmployee searchEmployeeBySalary(const Employee table[], int sizeTable, double nameToFind); 
+    PtrToEmployee searchEmployeeByPhone(const Employee table[], int sizeTable, char * targetPhone);
+    
  
     //defined in employeeTable.c 
     extern Employee EmployeeTable[];     
@@ -29,6 +32,20 @@ int main(void){
         printf("Employee Tony Bobcat is in record %d\n", matchPtr - EmployeeTable); 
     else 
         printf("Employee Tony Bobcat is NOT found in the record\n"); 
-     
+    
+    matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 6.34); 
+
+    if (matchPtr != NULL) 
+        printf("Employee Salary is in record %ld\n", matchPtr - EmployeeTable); 
+    else 
+        printf("Employee Salary is NOT found in the record\n"); 
+
+    matchPtr = searchEmployeeByPhone(EmployeeTable, EmployeeTableEntries, "909-555-1235"); 
+
+    if (matchPtr != NULL) 
+        printf("Employee Phone is in record %ld\n", matchPtr - EmployeeTable); 
+    else 
+        printf("Employee Phone is NOT found in the record\n"); 
+
     return EXIT_SUCCESS; 
 } 
--- EmployeeOne.c
+++ EmployeeOne.c
@@ -1,5 +1,5 @@
 #include <string.h> 
-#include "employee.h" 
+#include "Employee.h" 
 PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int tableSize, long targetNumber){ 
     const PtrToConstEmployee endPtr = ptr + tableSize; 
     for(; ptr < endPtr; ptr++)  //search until end of table  ptr++ will increment by what?? 
@@ -16,6 +16,26 @@ PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char *
     for(; ptr < endPtr; ptr++) 
     { 
         if(strcmp(ptr->name,targetName) == 0) 
+            return (PtrToEmployee) ptr; 
+    } 
+    return NULL; 
+}
+//Compare targetPhone with phone char*
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int tableSize, char * targetPhone){ 
+    const PtrToConstEmployee endPtr = ptr + tableSize; 
+    for(; ptr < endPtr; ptr++) 
+    { 
+        if(strcmp(ptr->phone,targetPhone) == 0) 
+            return (PtrToEmployee) ptr; 
+    } 
+    return NULL; 
+}
+//Compare 2 longs 
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int tableSize, double targetSalary){ 
+    const PtrToConstEmployee endPtr = ptr + tableSize; 
+    for(; ptr < endPtr; ptr++) 
+    { 
+        if(ptr->salary == targetSalary) 
             return (PtrToEmployee) ptr; 
     } 
     return NULL; 
